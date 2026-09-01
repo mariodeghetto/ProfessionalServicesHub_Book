f2af37ac69de67999aced6528c756c2021199655
Mario De Ghetto <mario@deghetto.it>
2026-09-01 21:48:19 +0200

    Add engagement and activity persistence model

--- ProfessionalServicesHub/Infrastructure/Data/ApplicationDbContext.cs
+++ ProfessionalServicesHub/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ProfessionalServicesHub.Domain.Clients;
+using ProfessionalServicesHub.Domain.Work;
 
 namespace ProfessionalServicesHub.Infrastructure.Data;
 
@@ -7,6 +8,10 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
     : DbContext(options)
 {
     public DbSet<Client> Clients => Set<Client>();
+
+    public DbSet<Engagement> Engagements => Set<Engagement>();
+
+    public DbSet<WorkActivity> WorkActivities => Set<WorkActivity>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -30,5 +35,46 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
             .IsUnique();
 
         client.HasIndex(x => x.Name);
+
+        var engagement = modelBuilder.Entity<Engagement>();
+
+        engagement.Property(x => x.Code)
+            .HasMaxLength(30)
+            .IsRequired();
+
+        engagement.Property(x => x.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        engagement.HasIndex(x => x.Code)
+            .IsUnique();
+
+        engagement.HasOne(x => x.Client)
+            .WithMany()
+            .HasForeignKey(x => x.ClientId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        var workActivity = modelBuilder.Entity<WorkActivity>();
+
+        workActivity.Property(x => x.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        workActivity.Property(x => x.Description)
+            .HasMaxLength(2000);
+
+        workActivity.Property(x => x.Assignee)
+            .HasMaxLength(120);
+
+        workActivity.HasIndex(x => new
+        {
+            x.EngagementId,
+            x.Status
+        });
+
+        workActivity.HasOne(x => x.Engagement)
+            .WithMany(x => x.Activities)
+            .HasForeignKey(x => x.EngagementId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
