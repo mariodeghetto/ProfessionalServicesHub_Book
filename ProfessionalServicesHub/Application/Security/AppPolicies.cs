bdbb45ddba1e8aeb8198bed26cee7e0ccccfe691
Mario De Ghetto <mario@deghetto.it>
2026-09-03 18:29:42 +0200

    Add Chapter 11 security foundation

---
+++ ProfessionalServicesHub/Application/Security/AppPolicies.cs
@@ -0,0 +1,10 @@
+namespace ProfessionalServicesHub.Application.Security;
+
+public static class AppPolicies
+{
+    public const string ManageConfiguration =
+        "ManageConfiguration";
+
+    public const string DispatchWork =
+        "DispatchWork";
+}
